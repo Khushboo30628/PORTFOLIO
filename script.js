9ce64e2abb9a4974a8bb3623267e1e5c9062b07a
Khushboo30628 <khushbooabdullah86@gmail.com>
2025-12-24 15:34:24 +0530

    updated todat

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const m = document.getElementById('mobileMenu');
-    m.style.display = m.style.display === 'flex' ? 'none' : 'flex';
-}
-
-document.getElementById('year').textContent = new Date().getFullYear();
-
-
